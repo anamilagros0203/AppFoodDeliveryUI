c0603b0bfabd844957076f95c2b41b430356b5b8
Paolo <Paolo@DESKTOP-EO3FFOD>
2021-09-24 17:32:06 -0500

    Agregar archivos de proyecto.

---
+++ AppFoodDeliveryUI/Views/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppFoodDeliveryUI.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnToSecondPage.Clicked += (sender, e) =>
+             {
+                 Navigation.PushAsync(new DeliveryPage());
+             };
+        }
+    }
+}
